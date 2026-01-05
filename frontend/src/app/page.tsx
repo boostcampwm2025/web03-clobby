144704fcc03980b626fc7bf5c42665b78f91daf7
seorang42 <seorang42@naver.com>
2026-01-05 16:42:50 +0900

    ✂️ Remove: 프로젝트 파일 정리
    Workspace 관련 파일을 제외한 이전 프로젝트의 파일을 정리하였습니다.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,6 @@ import { CopyIcon } from '@/assets/icons/common';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <CopyIcon className="h-20 w-20 text-red-500" />
-    </div>
+    <div className="flex min-h-screen items-center justify-center">Hello</div>
   );
 }
