a0fdd1717fd54c02874e044e893011f3aa0e153b
seorang42 <seorang42@naver.com>
2026-01-06 03:21:11 +0900

    🎨 Style: 공통 모달 UI 구현

---
+++ frontend/src/components/common/Modal.tsx
@@ -0,0 +1,73 @@
+interface ModalProps {
+  title: string;
+  cancelText: string;
+  onCancel: () => void;
+  confirmText?: string;
+  onConfirm?: () => void;
+  isLightMode?: boolean;
+  children?: React.ReactNode;
+
+  // Confirm 버튼을 빨간색으로 변경
+  isWarning?: boolean;
+}
+
+export default function Modal({
+  title,
+  cancelText,
+  onCancel,
+  confirmText,
+  onConfirm,
+  isLightMode,
+  children,
+  isWarning,
+}: ModalProps) {
+  return (
+    <dialog
+      className={`fixed top-1/2 left-1/2 flex min-h-40 min-w-80 -translate-1/2 flex-col gap-4 rounded-lg border p-6 ${isLightMode ? styles.dialog.light : styles.dialog.dark}`}
+    >
+      <h2
+        className={`font-bold ${isLightMode ? styles.h2.light : styles.h2.dark}`}
+      >
+        {title}
+      </h2>
+      <section
+        className={`flex-1 ${isLightMode ? styles.section.light : styles.section.dark}`}
+      >
+        {children}
+      </section>
+      <div className="flex w-full justify-end gap-3">
+        <button
+          onClick={onCancel}
+          className={`rounded-sm px-3 py-1.5 text-sm font-bold ${isLightMode ? styles.cancelBtn.light : styles.cancelBtn.dark}`}
+        >
+          {cancelText}
+        </button>
+        {confirmText && onConfirm && (
+          <button
+            onClick={onConfirm}
+            className={`rounded-sm px-3 py-1.5 text-sm font-bold ${isLightMode ? styles.confirmBtn.light : styles.confirmBtn.dark} ${isWarning ? (isLightMode ? 'bg-red-600!' : 'bg-red-700!') : ''}`}
+          >
+            {confirmText}
+          </button>
+        )}
+      </div>
+    </dialog>
+  );
+}
+
+const styles = {
+  dialog: {
+    light: 'bg-white border-neutral-200',
+    dark: 'bg-neutral-600 border-neutral-500',
+  },
+  h2: { light: 'text-neutral-900', dark: 'text-neutral-50' },
+  section: { light: 'text-neutral-600', dark: 'text-neutral-200' },
+  cancelBtn: {
+    light: 'bg-neutral-200 text-neutral-900',
+    dark: 'bg-neutral-500 text-neutral-50',
+  },
+  confirmBtn: {
+    light: 'bg-sky-600 text-neutral-50',
+    dark: 'bg-sky-700 text-neutral-50',
+  },
+};
