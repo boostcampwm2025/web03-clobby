c9cf6c71534a13b26ad4bdf4a13febe12df96d6c
KimDwDev <dongwan123456789@gmail.com>
2026-01-07 19:44:23 +0900

    🚚 Chore: module에 유스케이스 정리하기

---
+++ rep/main_backend/src/3-2.presentation/websocket/sfu/sfu.module.ts
@@ -0,0 +1,60 @@
+import { ConnectRoomUsecase, DisconnectRoomUsecase } from "@app/room/commands/usecase";
+import { AuthModule } from "@present/http/auth/auth.module";
+import { Module } from "@nestjs/common";
+import { SelectRoomDataFromMysql } from "@infra/db/mysql/room/room.inbound";
+import { CompareRoomArgonHash } from "./sfu.interface";
+import { SelectRoomInfoFromRedis } from "@infra/cache/redis/room/room.inbound";
+import { DeleteHardRoomParticipantInfoDataToMysql, InsertRoomParticipantInfoDataToMysql, UpdateRoomParticipantInfoToMysql } from "@infra/db/mysql/room/room.outbound";
+import { DeleteRoomDatasToRedis, InsertRoomDatasToRedis } from "@infra/cache/redis/room/room.outbound";
+
+
+@Module({
+  imports : [
+    AuthModule // jwt를 사용하기 위해서
+  ],
+  providers : [
+    // sfu 자체적인 모듈
+    CompareRoomArgonHash,
+
+    // usecase 모아두기
+    {
+      provide : ConnectRoomUsecase,
+      useFactory : (
+        selectRoomDataFromDb : SelectRoomDataFromMysql,
+        compareRoomPasswordHash : CompareRoomArgonHash,
+        selectRoomInfoDataFromCache : SelectRoomInfoFromRedis,
+        insertRoomParticipantInfoDataToDb : InsertRoomParticipantInfoDataToMysql,
+        insertRoomDatasToCache : InsertRoomDatasToRedis,
+        deleteRoomParticipantInfoDataToDb : DeleteHardRoomParticipantInfoDataToMysql
+      ) => {  
+        return new ConnectRoomUsecase({
+          selectRoomDataFromDb, compareRoomPasswordHash, selectRoomInfoDataFromCache, insertRoomParticipantInfoDataToDb, insertRoomDatasToCache, deleteRoomParticipantInfoDataToDb
+        })
+      },
+      inject : [
+        SelectRoomDataFromMysql,
+        CompareRoomArgonHash,
+        SelectRoomInfoFromRedis,
+        InsertRoomParticipantInfoDataToMysql,
+        InsertRoomDatasToRedis,
+        DeleteHardRoomParticipantInfoDataToMysql
+      ]
+    },
+    {
+      provide : DisconnectRoomUsecase,
+      useFactory : (
+        updateRoomParticipantInfoToDb : UpdateRoomParticipantInfoToMysql,
+        deleteRoomDataToCache : DeleteRoomDatasToRedis
+      ) => {
+        return new DisconnectRoomUsecase({
+          updateRoomParticipantInfoToDb, deleteRoomDataToCache
+        })
+      },
+      inject : [
+        UpdateRoomParticipantInfoToMysql,
+        DeleteRoomDatasToRedis
+      ]
+    }
+  ]
+})
+export class SfuModule {};
