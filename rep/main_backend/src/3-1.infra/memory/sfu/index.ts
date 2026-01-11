8e548acc53ab86c1fd42eba823da7beec0c4ac7c
KimDwDev <dongwan123456789@gmail.com>
2026-01-12 04:47:58 +0900

    📍 Feat: consumer repo 작성 후 등록

--- rep/main_backend/src/3-1.infra/memory/sfu/index.ts
+++ rep/main_backend/src/3-1.infra/memory/sfu/index.ts
@@ -2,3 +2,4 @@ export * from "./room-create-lock.repo";
 export * from "./room-router.repo"
 export * from "./transport-repo";
 export * from "./producer-repo.repo";
+export * from "./consumer-repo.repo"
