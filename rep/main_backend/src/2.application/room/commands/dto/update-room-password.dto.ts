a530010e448aeffb9bd1630245d017fc224a823e
KimDwDev <dongwan123456789@gmail.com>
2026-01-28 07:48:23 +0900

    🚚 Chore: 방에 비밀번호를 변경하는 usecase 작성

---
+++ rep/main_backend/src/2.application/room/commands/dto/update-room-password.dto.ts
@@ -0,0 +1,14 @@
+
+
+
+export type UpdateRoomPasswordDto = {
+  user_id : string; // payload에서 가져와야 한다.
+  new_password : string;
+  code : string;
+  // 과거 password를 물어볼수도 있다.
+};
+
+export type UpdateRoomInfoResult = {
+  room_id : string;
+  prev_password : string | null; // 후에 필요할 수도 있다. 
+};
