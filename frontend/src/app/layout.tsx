619100fb7d457f4a7fb4a3f4208877d265d03ee7
chamny20 <dge3179@ajou.ac.kr>
2026-02-02 14:12:40 +0900

    🌈 Update: clarity id를 포함한 스크립트 추가
    clarity 모니터링용

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
-import './globals.css';
 import Script from 'next/script';
+import './globals.css';
 
 const suit = localFont({
   src: '../assets/fonts/SUIT-Variable.woff2',
@@ -34,19 +34,22 @@ export default function RootLayout({
         {children}
 
         <div id="modal-root" />
-        <Script
-          id="microsoft-clarity-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
+
+        {process.env.NEXT_PUBLIC_CLARITY_ID && (
+          <Script
+            id="microsoft-clarity-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
               (function(c,l,a,r,i,t,y){
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
               })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
             `,
-          }}
-        />
+            }}
+          />
+        )}
       </body>
     </html>
   );
